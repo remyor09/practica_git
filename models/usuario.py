ebc33c7660d57f7006d28cb33c6a73ac8c0a2ac7
Roymer Romero Algarin <rromeroalg@misena.edu.co>
2021-01-12 13:54:00 -0500

    Modificacion de clase Usuario.py agregando conteo de instancias creadas

--- models/usuario.py
+++ models/usuario.py
@@ -1,4 +1,20 @@
 class Usuario:
     
-    def __init__(self, name):
+    id = 0
+
+    def __init__(self):
+        Usuario.id += 1
+
+    def setName(self, name):
         self.name = name
+
+usuario = Usuario()
+nombre = input("Nombre: ")
+usuario.setName(nombre)
+print(usuario.id)
+
+usuario2 = Usuario()
+print(usuario2.id)
+
+usuario3 = Usuario()
+print(usuario3.id)
